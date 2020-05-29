c3aafae678f34f2f606311f8ec9b868f0b6e6248
Kushuh <kuzanisu@gmail.com>
2020-05-29 13:47:31 +0200

    update options

--- config/options.go
+++ config/options.go
@@ -8,4 +8,5 @@ type Options struct {
 	QueryString string `json:"query_string"`
 	Labels map[string][]string `json:"labels"`
 	LabelsOptions map[string]LabelOption `json:"labels_options"`
+	Config Config `json:"config"`
 }
